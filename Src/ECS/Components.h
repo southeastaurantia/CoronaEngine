f04d0304c4d886617b7e3323924113d9c98d5a0a
JOPLOPOL <95957645+JOPLOPOL@users.noreply.github.com>
2025-08-07 14:25:27 +0800

    添加动画组件

--- Src/ECS/Components.h
+++ Src/ECS/Components.h
@@ -7,7 +7,25 @@
 namespace ECS::Components
 {
     struct ResLoadedTag{};
+    
+    struct Camera
+    {
+        float fov = 45.0f;
+        ktm::fvec3 pos = ktm::fvec3(1.0f, 1.0f, 1.0f);
+        ktm::fvec3 forward = ktm::fvec3(-1.0f, -1.0f, -1.0f);
+        ktm::fvec3 worldUp = ktm::fvec3(0.0f, 1.0f, 0.0f);
+    };
+    
+    struct SunLight
+    {
+        ktm::fvec3 direction;
+    };
 
+    struct Actors
+    {
+        std::vector<entt::entity> actors;
+    };
+    
     struct ActorPose
     {
         ktm::fvec3 transform = ktm::fvec3(0.0f, 0.0f, 0.0f);
@@ -28,25 +46,10 @@ namespace ECS::Components
         HardwareBuffer matrices;
     };
 
-    struct ImageHost
+    struct Meshes
     {
-        std::string path;            ///< 纹理文件路径
-        unsigned char *data;         ///< 纹理数据指针
-        int width, height, channels; ///< 纹理宽、高、通道数
-    };
-
-    struct ImageDevice
-    {
-        HardwareImage image;
-    };
-
-    struct MaterialParams
-    {
-        ktm::fvec3 baseColor;
-        float roughness;
-        float metallic;
-        float specular;
-        float transmission;
+        std::vector<entt::entity> meshes;
+        std::string path;
     };
 
     struct MeshHost
@@ -59,11 +62,6 @@ namespace ECS::Components
         std::vector<float> boneWeights;
     };
 
-    struct Material
-    {
-        entt::entity material;
-    };
-
     struct MeshDevice
     {
         HardwareBuffer indicesBuffer;
@@ -72,6 +70,20 @@ namespace ECS::Components
         HardwareBuffer texCoordsBuffer;
         HardwareBuffer boneIndicesBuffer;
         HardwareBuffer boneWeightsBuffer;
+    };
+
+    struct Material
+    {
+        entt::entity material;
+    };
+
+    struct MaterialParams
+    {
+        ktm::fvec3 baseColor;
+        float roughness;
+        float metallic;
+        float specular;
+        float transmission;
     };
 
     struct BaseColorTexture
@@ -89,32 +101,80 @@ namespace ECS::Components
         entt::entity texture;
     };
 
-    struct Meshes
+    struct ImageHost
     {
-        std::vector<entt::entity> meshes;
-        std::string path;
+        std::string path;            ///< 纹理文件路径
+        unsigned char *data;         ///< 纹理数据指针
+        int width, height, channels; ///< 纹理宽、高、通道数
     };
 
-    struct Camera
+    struct ImageDevice
     {
-        float fov = 45.0f;
-        ktm::fvec3 pos = ktm::fvec3(1.0f, 1.0f, 1.0f);
-        ktm::fvec3 forward = ktm::fvec3(-1.0f, -1.0f, -1.0f);
-        ktm::fvec3 worldUp = ktm::fvec3(0.0f, 1.0f, 0.0f);
-    };
-
-    struct SunLight
-    {
-        ktm::fvec3 direction;
-    };
-
-    struct Actors
-    {
-        std::vector<entt::entity> actors;
+        HardwareImage image;
     };
 
     struct Model
     {
         entt::entity model;
     };
+
+    struct KeyPosition
+    {
+        double time;
+        ktm::fvec3 position;
+    };
+
+    struct KeyRotation
+    {
+        double time;
+        ktm::fquat rotation;
+    };
+
+    struct KeyScale
+    {
+        double time;
+        ktm::fvec3 scale;
+    };
+
+    struct Bone
+    {
+        std::vector<KeyPosition>    keyPositions;
+        std::vector<KeyRotation>    keyRotations;
+        std::vector<KeyScale>       keyScales;
+        int NumPositions;
+        int NumRotations;
+        int NumScales;
+        std::string name;
+        int id;
+    };
+
+    struct AssimpNodeData
+    {
+        std::string name;
+        ktm::fmat4x4 transformation;
+        int chilenCount;
+        std::vector<AssimpNodeData> children;
+    };
+
+    struct SkeletalAnimation
+    {
+        double duration;
+        double ticksPerSecond;
+        std::vector<Bone> bones;
+        AssimpNodeData rootNode;
+    };
+
+    struct BoneInfo
+    {
+        int id;
+        ktm::fmat4x4 offsetMatrix;
+    };
+
+    struct Animations
+    {
+        std::vector<SkeletalAnimation> skeletalAnimations;
+        std::map<std::string, BoneInfo> boneInfoMap; 
+    };
+
+
 } // namespace ECS::Components
