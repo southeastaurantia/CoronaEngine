5a910c63059be65c1e4afcf9baff554dd6f83953
与湫风起 <472266503@qq.com>
2025-09-09 19:41:12 +0800

    Update #封装一下全局数据缓存结构，提供线程安全/快速的访问/修改方法，添加示例用法
    Signed-off-by: 与湫风起 <472266503@qq.com>

--- Examples/main.cpp
+++ Examples/main.cpp
@@ -1,10 +1,14 @@
-﻿#include <Core/IO/ResMgr.h>
+﻿#include "Core/SafeDataCache.h"
+
+#include <Core/IO/ResMgr.h>
 #include <Core/Logger.h>
+#include <chrono>
 #include <iostream>
 
 class Texture final : public Corona::Resource
 {
   public:
+    int id;
     int width;
     int height;
 
@@ -24,13 +28,13 @@ class TextureLoader final : public Corona::ResourceLoader<Texture>
         // 直接修改传入的 handle 指向的对象
         handle->width = 10;
         handle->height = 20;
-        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
         return true;
     }
 };
 
 int main()
 {
+    // LOG_INFO是耗时操作
 
     Corona::ResMgr<Texture>::register_loader<TextureLoader>();
 
@@ -42,7 +46,92 @@ int main()
     LOG_INFO("Player2 texture {}:{}", player2_texture->width, player2_texture->height);
     LOG_INFO("Player3 texture {}:{}", player3_texture->width, player3_texture->height);
 
+    Corona::SafeDataCache<Texture> texture_caches;
+    std::unordered_set<uint64_t> keys2{}, keys3{};
+
+    for (int i = 0; i < 1000; ++i)
+    {
+        int val = i + 1;
+        auto texture = std::make_shared<Texture>();
+        texture->id = i;
+        texture->width = val;
+        texture->height = val * 2;
+        texture_caches.insert(i, texture);
+        keys2.insert(i);
+        keys3.insert(i);
+    }
+
+    std::atomic<uint64_t> total_spend_time = 0;
+
+    bool used_safe_loop_foreach = true;
+
+    std::thread worker2 = std::thread([&] {
+        auto queue = std::queue<uint64_t>();
+        auto now = std::chrono::high_resolution_clock::now();
+        if (!used_safe_loop_foreach)
+        {
+            for (int i = 0; i < texture_caches.size(); ++i)
+            {
+                texture_caches.modify(i, [&](const std::shared_ptr<Texture> &texture) {
+                    texture->width = 555;
+                    texture->height = 555;
+                    LOG_INFO("Worker2 process texture {}", texture->id);
+                });
+            }
+        }
+        else
+        {
+            texture_caches.safe_loop_foreach(keys2, [&](const std::shared_ptr<Texture> &texture) {
+                texture->width = 555;
+                texture->height = 555;
+                LOG_INFO("Worker2 process texture {}", texture->id);
+            });
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto spend = std::chrono::duration_cast<std::chrono::microseconds>(end - now).count();
+        total_spend_time.fetch_add(spend);
+        LOG_INFO("Worker2 finish: {}us, use safe loop foreach: {}", spend, used_safe_loop_foreach ? "true" : "false");
+    });
+
+    std::thread worker3 = std::thread([&] {
+        auto queue = std::queue<uint64_t>();
+        auto now = std::chrono::high_resolution_clock::now();
+        if (!used_safe_loop_foreach)
+        {
+            for (int i = 0; i < texture_caches.size(); ++i)
+            {
+                texture_caches.modify(i, [&](const std::shared_ptr<Texture> &texture) {
+                    texture->width = 666;
+                    texture->height = 666;
+                    LOG_INFO("Worker3 process texture {}", texture->id);
+                });
+            }
+        }
+        else
+        {
+            texture_caches.safe_loop_foreach(keys3, [&](const std::shared_ptr<Texture> &texture) {
+                texture->width = 666;
+                texture->height = 666;
+                LOG_INFO("Worker3 process texture {}", texture->id);
+            });
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        auto spend = std::chrono::duration_cast<std::chrono::microseconds>(end - now).count();
+        total_spend_time.fetch_add(spend);
+        LOG_INFO("Worker3 finish: {}us, use safe loop foreach: {}", spend, used_safe_loop_foreach ? "true" : "false");
+    });
+
+    auto texture = texture_caches.get(5);
+    // texture->width = 5 // not allowed, used texture_caches.modify
+    LOG_INFO("Texture get id 5, {}:{}", texture->width, texture->height);
+
+    worker2.join();
+    worker3.join();
+
     std::cin.get();
+
+    LOG_INFO("Size = {}, Total spend: {}us", texture_caches.size(), total_spend_time.load());
 
     return 0;
 }
---
+++ Src/Core/SafeDataCache.h
@@ -0,0 +1,137 @@
+//
+// Created by 47226 on 2025/9/9.
+//
+
+#ifndef CORONAENGINE_SAFEDATACACHE_H
+#define CORONAENGINE_SAFEDATACACHE_H
+#include "Logger.h"
+#include "oneapi/tbb/concurrent_hash_map.h"
+
+#include <cstdint>
+#include <queue>
+#include <typeindex>
+#include <unordered_set>
+
+namespace Corona
+{
+    template <typename TData>
+    class SafeDataCache final
+    {
+      public:
+        using id_type = uint64_t;
+        using mutexes_type = tbb::concurrent_hash_map<id_type, std::shared_ptr<std::mutex>>;
+        using caches_type = tbb::concurrent_hash_map<id_type, std::shared_ptr<TData>>;
+
+        auto size() const
+        {
+            return data_cache.size();
+        }
+
+        bool insert(const id_type &id, std::shared_ptr<TData> data)
+        {
+            if (!data_cache.emplace(id, data))
+            {
+                LOG_WARN("Data id:{} type:{} insert failed", id, std::type_index(typeid(TData)).name());
+                return false;
+            }
+            foreach_mutex.emplace(id, std::make_shared<std::mutex>());
+            return true;
+        }
+
+        bool erase(const id_type &id)
+        {
+            if (!data_cache.erase(id))
+            {
+                LOG_WARN("Data id:{} type:{} erase failed", id, std::type_index(typeid(TData)).name());
+                return false;
+            }
+            foreach_mutex.erase(id);
+            return true;
+        }
+
+        std::shared_ptr<const TData> get(const id_type &id) const
+        {
+            typename caches_type::accessor data_it;
+            if (!data_cache.find(data_it, id))
+            {
+                LOG_WARN("Data id:{} type:{} get failed", id, std::type_index(typeid(TData)).name());
+                return nullptr;
+            }
+            return data_it->second;
+        }
+
+        bool modify(const id_type &id, std::function<void(std::shared_ptr<TData>)> callback)
+        {
+            typename caches_type::accessor data_it;
+            mutexes_type::accessor foreach_it;
+
+            if (!data_cache.find(data_it, id))
+            {
+                LOG_WARN("Data id:{} type:{} modify failed, data not found", id, std::type_index(typeid(TData)).name());
+                return false;
+            }
+            if (!foreach_mutex.find(foreach_it, id))
+            {
+                LOG_WARN("Data id:{} type:{} modify failed, mutex not found", id, std::type_index(typeid(TData)).name());
+                return false;
+            }
+            std::lock_guard lock(*foreach_it->second);
+            callback(data_it->second);
+            return true;
+        }
+
+        void safe_loop_foreach(const std::unordered_set<id_type> &data_ids, std::function<void(std::shared_ptr<TData>)> callback)
+        {
+            std::queue<id_type> unhandled_data_ids;
+            for (auto const &id : data_ids)
+            {
+                typename caches_type::accessor data_it;
+                mutexes_type::accessor foreach_it;
+                if (!data_cache.find(data_it, id))
+                {
+                    continue;
+                }
+                if (!foreach_mutex.find(foreach_it, id))
+                {
+                    continue;
+                }
+                if (foreach_it->second->try_lock())
+                {
+                    callback(data_it->second);
+                    foreach_it->second->unlock();
+                    continue;
+                }
+                unhandled_data_ids.push(id);
+            }
+
+            while (!unhandled_data_ids.empty())
+            {
+                auto const &id = unhandled_data_ids.front();
+                unhandled_data_ids.pop();
+                typename caches_type::accessor data_it;
+                mutexes_type::accessor foreach_it;
+                if (!data_cache.find(data_it, id))
+                {
+                    continue;
+                }
+                if (!foreach_mutex.find(foreach_it, id))
+                {
+                    continue;
+                }
+                if (foreach_it->second->try_lock())
+                {
+                    callback(data_it->second);
+                    foreach_it->second->unlock();
+                    continue;
+                }
+                unhandled_data_ids.push(id);
+            }
+        }
+
+      private:
+        caches_type data_cache{};
+        mutexes_type foreach_mutex{};
+    };
+} // namespace Corona
+
+#endif // CORONAENGINE_SAFEDATACACHE_H
