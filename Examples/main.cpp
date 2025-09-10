8db4ebe4197bdcd8ec5072265e726450b4e615c2
JOPLOPOL <95957645+JOPLOPOL@users.noreply.github.com>
2025-09-10 22:39:46 +0800

    重命名文件夹

--- Examples/main.cpp
+++ Examples/main.cpp
@@ -1,14 +1,5 @@
 ﻿#include "Core/Engine.h"
-#include "Core/Thread/SafeDataCache.h"
-#include "Multimedia/Animation/AnimationSystemDefault.hpp"
-#include "Multimedia/Audio/AudioSystemDefault.hpp"
-#include "Multimedia/Display/DisplaySystemDefault.hpp"
-#include "Multimedia/Rendering/RenderingSystemDefault.hpp"
 
-#include <Core/IO/ResMgr.h>
-#include <Core/Logger.h>
-#include <chrono>
-#include <iostream>
 
 int main()
 {
