f00d201af66c1ddc92702209ca2e4fadb606a043
JOPLOPOL <95957645+JOPLOPOL@users.noreply.github.com>
2025-09-10 23:45:45 +0800

    补充遗漏文件

---
+++ Src/Multimedia/Audio/AudioSystemDefault.h
@@ -0,0 +1,39 @@
+//
+// Created by 47226 on 2025/9/5.
+//
+
+#ifndef CORONAENGINE_AUDIOSYSTEMDEFAULT_HPP
+#define CORONAENGINE_AUDIOSYSTEMDEFAULT_HPP
+#include "Core/Engine.h"
+#include "Multimedia/BaseMultimediaSystem.hpp"
+
+namespace Corona
+{
+
+    class AudioSystemDefault final : public BaseMultimediaSystem
+    {
+      public:
+
+        const char *name() override;
+
+      public:
+        explicit AudioSystemDefault();
+        ~AudioSystemDefault() override;
+
+      public:
+        void start() override;
+        void tick() override;
+        void stop() override;
+
+      private:
+        std::unordered_set<DataCache::id_type> data_keys;   // 全局DataCache的所有key
+        std::queue<DataCache::id_type> unhandled_data_keys; // 当前帧未处理的key
+        std::thread audioThread;
+        std::atomic<bool> running;
+
+        void processAudio(DataCache::id_type id);
+    };
+
+} // namespace Corona
+
+#endif // CORONAENGINE_AUDIOSYSTEMDEFAULT_HPP
