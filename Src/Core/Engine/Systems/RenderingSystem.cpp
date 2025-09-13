f874f368659e5fbb313b614876f36a4198d85830
JOPLOPOL <95957645+JOPLOPOL@users.noreply.github.com>
2025-09-13 16:29:16 +0800

    添加render需要的参数

--- Src/Core/Engine/Systems/RenderingSystem.cpp
+++ Src/Core/Engine/Systems/RenderingSystem.cpp
@@ -12,6 +12,7 @@ RenderingSystem::RenderingSystem()
     : ThreadedSystem("RenderingSystem")
 {
     Engine::Instance().AddQueue(name(), std::make_unique<SafeCommandQueue>());
+    init();
 }
 
 void RenderingSystem::onStart()
@@ -37,6 +38,17 @@ void RenderingSystem::onStop()
 {
 }
 
+void RenderingSystem::init()
+{
+    gbufferPostionImage = HardwareImage(gbufferSize, ImageFormat::RGBA16_FLOAT, ImageUsage::StorageImage);
+    gbufferBaseColorImage = HardwareImage(gbufferSize, ImageFormat::RGBA16_FLOAT, ImageUsage::StorageImage);
+    gbufferNormalImage = HardwareImage(gbufferSize, ImageFormat::RGBA16_FLOAT, ImageUsage::StorageImage);
+    gbufferMotionVectorImage = HardwareImage(gbufferSize, ImageFormat::RG32_FLOAT, ImageUsage::StorageImage);
+
+    uniformBuffer = HardwareBuffer(sizeof(UniformBufferObject), BufferUsage::UniformBuffer);
+    gbufferUniformBuffer = HardwareBuffer(sizeof(gbufferUniformBufferObject), BufferUsage::UniformBuffer);
+}
+
 void RenderingSystem::processRender(uint64_t /*id*/)
 {
     updateEngine();
@@ -50,12 +62,64 @@ void RenderingSystem::updateEngine()
 }
 void RenderingSystem::gbufferPipeline()
 {
-    // TODO: 设置视图投影、写入 G-Buffer
+    // uniformBufferObjects.eyePosition = scene->getCamera().pos;
+    // uniformBufferObjects.eyeDir = ktm::normalize(scene->getCamera().forward);
+    //
+    // uniformBufferObjects.eyeViewMatrix = ktm::look_at_lh(uniformBufferObjects.eyePosition, ktm::normalize(camera.forward), camera.worldUp);
+    // uniformBufferObjects.eyeProjMatrix = ktm::perspective_lh(ktm::radians(45.0f), (float)gbufferSize.x / (float)gbufferSize.y, 0.1f, 100.0f);
+    //
+    // gbufferUniformBufferObjects.viewProjMatrix = uniformBufferObjects.eyeProjMatrix * uniformBufferObjects.eyeViewMatrix;
+    // gbufferUniformBuffer.copyFromData(&gbufferUniformBufferObjects, sizeof(gbufferUniformBufferObjects));
+    //
+    // std::set<CabbageEngine::Actor *> actors = scene->getActors();
+    // for (auto &actor : actors)
+    // {
+    //     ktm::fmat4x4 actorMatrix = actor->getActorMatrix();
+    //     gbufferPipleLine["pushConsts.modelMatrix"] = actorMatrix;
+    //
+    //     HardwareBuffer bonesMatrixBuffer = actor->getBonesMatrixBuffer();
+    //     gbufferPipleLine["pushConsts.uniformBufferIndex"] = gbufferUniformBuffer.storeDescriptor();
+    //     gbufferPipleLine["pushConsts.boneIndex"] = bonesMatrixBuffer.storeDescriptor();
+    //
+    //     std::vector<CabbageEngine::Actor::MeshDeviceData> deviceMeshes = actor->getDeviceMeshes();
+    //     for (auto &geom : deviceMeshes)
+    //     {
+    //         gbufferPipleLine["inPosition"] = geom.pointsBuffer;
+    //         gbufferPipleLine["inNormal"] = geom.normalsBuffer;
+    //         gbufferPipleLine["inTexCoord"] = geom.texCoordsBuffer;
+    //         gbufferPipleLine["boneIndexes"] = geom.boneIndexesBuffer;
+    //         gbufferPipleLine["jointWeights"] = geom.boneWeightsBuffer;
+    //         gbufferPipleLine["pushConsts.textureIndex"] = geom.textureIndex;
+    //
+    //         gbufferPipleLine.recordGeomMesh(geom.indexBuffer);
+    //     }
+    // }
+    //
+    // gbufferPipleLine["gbufferPostion"] = gbufferPostionImage;
+    // gbufferPipleLine["gbufferBaseColor"] = gbufferBaseColorImage;
+    // gbufferPipleLine["gbufferNormal"] = gbufferNormalImage;
+    // gbufferPipleLine["gbufferMotionVector"] = gbufferMotionVectorImage;
+    // gbufferPipleLine.executePipeline(gbufferSize);
     CE_LOG_DEBUG("RenderingSystem: gbufferPipeline executed");
 }
 void RenderingSystem::compositePipeline()
 {
-    // TODO: 合成阶段、灯光与最终输出
+    // rendererPipleLine["pushConsts.gbufferSize"] = gbufferSize;
+    // rendererPipleLine["pushConsts.gbufferPostionImage"] = gbufferPostionImage.storeDescriptor();
+    // rendererPipleLine["pushConsts.gbufferBaseColorImage"] = gbufferBaseColorImage.storeDescriptor();
+    // rendererPipleLine["pushConsts.gbufferNormalImage"] = gbufferNormalImage.storeDescriptor();
+    // rendererPipleLine["pushConsts.gbufferDepthImage"] = gbufferPipleLine.getDepthImage().storeDescriptor();
+    //
+    // rendererPipleLine["pushConsts.finalOutputImage"] = finalOutputImage.storeDescriptor();
+    //
+    // rendererPipleLine["pushConsts.sun_dir"] = ktm::normalize(sunDir);
+    //
+    // rendererPipleLine["pushConsts.lightColor"] = ktm::fvec3(23.47f, 21.31f, 20.79f);
+    //
+    // uniformBuffer.copyFromData(&uniformBufferObjects, sizeof(uniformBufferObjects));
+    // rendererPipleLine["pushConsts.uniformBufferIndex"] = uniformBuffer.storeDescriptor();
+    //
+    // rendererPipleLine.executePipeline(ktm::uvec3(gbufferSize.x / 8, gbufferSize.y / 8, 1));
     CE_LOG_DEBUG("RenderingSystem: compositePipeline executed");
 }
 
--- Src/Core/Engine/Systems/RenderingSystem.h
+++ Src/Core/Engine/Systems/RenderingSystem.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include "CabbageDisplayer.h"
 #include "Core/Engine/ThreadedSystem.h"
+#include "Pipeline/ComputePipeline.h"
+#include "Pipeline/RasterizerPipeline.h"
+#include "ktm/type_vec.h"
+
 #include <unordered_set>
+
 
 
 namespace Corona
@@ -25,7 +31,37 @@ namespace Corona
         // 迁移保留：全局DataCache的所有key集合（后续用于 foreach）
         std::unordered_set<uint64_t> data_keys_{};
 
+        struct UniformBufferObject
+        {
+            ktm::fvec3 lightPostion;
+            ktm::fmat4x4 lightViewMatrix;
+            ktm::fmat4x4 lightProjMatrix;
+
+            ktm::fvec3 eyePosition;
+            ktm::fvec3 eyeDir;
+            ktm::fmat4x4 eyeViewMatrix;
+            ktm::fmat4x4 eyeProjMatrix;
+        } uniformBufferObjects;
+
+        struct gbufferUniformBufferObject
+        {
+            ktm::fmat4x4 viewProjMatrix;
+        } gbufferUniformBufferObjects;
+
+        ktm::uvec2 gbufferSize = ktm::uvec2(1920, 1080);
+        HardwareImage gbufferPostionImage;
+        HardwareImage gbufferBaseColorImage;
+        HardwareImage gbufferNormalImage;
+        HardwareImage gbufferMotionVectorImage;
+
+        // RasterizerPipeline gbufferPipeline;
+        // ComputePipeline compositePipeline;
+
+        HardwareBuffer uniformBuffer;
+        HardwareBuffer gbufferUniformBuffer;
+
         // 对应旧版的渲染流程拆分
+        void init();
         void processRender(uint64_t id);
         void updateEngine();
         void gbufferPipeline();
