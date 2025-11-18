585881a7cf9dd3f5ce47c1cbca5b4f1c653220a0
GraphZou <906520989@qq.com>
2025-11-18 15:08:41 +0800

    测试版本

--- src/systems/optics/optics_system.cpp
+++ src/systems/optics/optics_system.cpp
@@ -51,8 +51,8 @@ bool OpticsSystem::initialize(Kernel::ISystemContext* ctx) {
         // visionDevice = RHIContext::instance().create_device("cuda");
         visionDevice.init_rtx();
         vision::Global::instance().set_device(&visionDevice);
-        vision::Global::instance().set_scene_path("E:\\CoronaResource\\examples\\assets\\test_vision\\render_scene\\kitchen");
-        auto str = "E:\\CoronaResource\\examples\\assets\\test_vision\\render_scene\\kitchen\\vision_scene.json";
+        vision::Global::instance().set_scene_path("E:\\CoronaTestScenes\\test_vision\\render_scene\\kitchen");
+        auto str = "E:\\CoronaTestScenes\\test_vision\\render_scene\\kitchen\\vision_scene.json";
         renderPipeline = vision::Importer::import_scene(str);
         renderPipeline->init();
         renderPipeline->prepare();
@@ -279,19 +279,19 @@ void OpticsSystem::optics_pipeline(float frame_count) const {
                                         << hardware_->computePipeline(1920 / 8, 1080 / 8, 1)
                                         << hardware_->executor.commit();
 
-#ifdef CORONA_ENABLE_VISION
-                    if (hardware_->displayers_.contains(reinterpret_cast<uint64_t>(camera.surface))) {
-                        renderPipeline->display(1 / 30);
-                        cudaViewBuffer->download_immediately(imageData.data());
-                        importedViewBuffer.copyFromData(imageData.data(), imageData.size() * sizeof(float) * 4);
-                        importedViewImage.copyFromBuffer(importedViewBuffer);
-                        hardware_->displayers_.at(reinterpret_cast<uint64_t>(camera.surface)).wait(hardware_->executor) << importedViewImage;
-                    }
-#else
+//#ifdef CORONA_ENABLE_VISION
+//                    if (hardware_->displayers_.contains(reinterpret_cast<uint64_t>(camera.surface))) {
+//                        renderPipeline->display(1 / 30);
+//                        cudaViewBuffer->download_immediately(imageData.data());
+//                        importedViewBuffer.copyFromData(imageData.data(), imageData.size() * sizeof(float) * 4);
+//                        importedViewImage.copyFromBuffer(importedViewBuffer);
+//                        hardware_->displayers_.at(reinterpret_cast<uint64_t>(camera.surface)).wait(hardware_->executor) << importedViewImage;
+//                    }
+//#else
                     if (hardware_->displayers_.contains(reinterpret_cast<uint64_t>(camera.surface))) {
                         hardware_->displayers_.at(reinterpret_cast<uint64_t>(camera.surface)).wait(hardware_->executor) << hardware_->finalOutputImage;
                     }
-#endif
+//#endif
                 });
             });
         }
