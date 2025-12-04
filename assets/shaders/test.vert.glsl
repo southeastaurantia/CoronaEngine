ff0a05f7be16dbbe14442afef72acc300cd84906
JOPLOPOL <95957645+JOPLOPOL@users.noreply.github.com>
2025-12-04 18:08:55 +0800

    Revert "fix"
    This reverts commit 90044798cac6a7e089158de6d953538c4603ff25.

--- assets/shaders/test.vert.glsl
+++ assets/shaders/test.vert.glsl
@@ -16,14 +16,12 @@ layout(set = 0, binding = 0) uniform UniformBufferObject
 
 layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec3 inNormal;
-layout(location = 2) in vec3 inColor;
-layout(location = 3) in vec2 inTexCoord;
+layout(location = 2) in vec2 inTexCoord;
 
 layout(location = 0) out vec3 fragPos;
 layout(location = 1) out vec3 fragNormal;
 layout(location = 2) out vec2 fragTexCoord;
 layout(location = 3) out vec2 fragMotionVector;
-layout(location = 4) out vec3 fragColor;
 
 
 void main() 
@@ -34,8 +32,6 @@ void main()
 
     fragTexCoord = inTexCoord;
     fragNormal = inNormal;
-
-    fragColor = inColor;
 
     fragMotionVector = vec2(0.0f);
 }
